7acc05d927f0b8d17895d0e8923b1193d5cb3cea
TimChen <a5040766@gmail.com>
2020-10-05 19:15:26 +0800

    add .net core Webapi homework
    add .net core Webapi homework

---
+++ .NETCore TodoList/CoreWebApi_TodoApi2/CoreWebApi_TodoApi2/CoreWebApi_TodoApi2/Data/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CoreWebApi_TodoApi2.Models;
+
+namespace CoreWebApi_TodoApi2.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)//Options Pattern選項模式
+        {
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
